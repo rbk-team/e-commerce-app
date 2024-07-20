1ca9fafeefb2679213f2c1eabbdc91595dc982bc
medban <bannourmohamedabdesslam@gmail.com>
2024-07-20 03:39:34 +0100

    first commit

---
+++ server/database/index.js
@@ -0,0 +1,7 @@
+const mysql = require("mysql2");
+const { Sequelize } = require("sequelize");
+
+// Your Database Queries Here!!
+
+// Don't forget to export your functions!
+module.exports = { Cows };
